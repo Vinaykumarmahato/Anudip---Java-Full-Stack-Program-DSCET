391694b58be0a92070adb9b32853a774fede3bf7
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-11-25 13:41:16 +0530

    initial commit

---
+++ Lab1_ANP_C9763_JavaBAsic/PrimitiveDataTypes.java
@@ -0,0 +1,37 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte byteValue = 100;     
+
+        short shortValue = 30000;
+
+        int intValue = 100000;  
+
+        long longValue = 100000L; 
+
+        float floatValue = 10.5f;  
+
+        double doubleValue = 20.99; 
+
+        char charValue = 'A';    
+        
+        
+        boolean booleanValue = true;  
+
+        System.out.println("Byte value: " + byteValue);
+
+        System.out.println("Short value: " + shortValue);
+
+        System.out.println("Int value: " + intValue);
+
+        System.out.println("Long value: " + longValue);
+
+        System.out.println("Float value: " + floatValue);
+
+        System.out.println("Double value: " + doubleValue);
+
+        System.out.println("Char value: " + charValue);
+        
+        System.out.println("Boolean value: " + booleanValue);
+    }
+}
