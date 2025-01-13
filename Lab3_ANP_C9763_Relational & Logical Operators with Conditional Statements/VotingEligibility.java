0c2ef60f1619907bceeceded1042786896653306
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2025-01-13 07:32:32 +0530

    Update VotingEligibility.java

--- Lab3_ANP_C9763_Relational & Logical Operators with Conditional Statements/VotingEligibility.java
+++ Lab3_ANP_C9763_Relational & Logical Operators with Conditional Statements/VotingEligibility.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class VotingEligibility {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
         
